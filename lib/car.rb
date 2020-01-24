7ca42d3a9c3a1c26bcde54ca16a51871b1facc39
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-24 10:27:46 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,6 +2,6 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle # used the < to inherit the Car class from Vehicle
   def go
-    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!" # this will overwrite the go method in the Vehicle class
   end  
 end
